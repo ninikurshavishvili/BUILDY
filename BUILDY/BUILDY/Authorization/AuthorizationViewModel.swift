1f17ea12153e85621895488f5d87a73fb56efd9b
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-01-19 18:52:53 +0400

    Add <add to cart> listener for unique Users

--- BUILDY/BUILDY/Authorization/AuthorizationViewModel.swift
+++ BUILDY/BUILDY/Authorization/AuthorizationViewModel.swift
@@ -8,12 +8,20 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestore
 
 class AuthorizationViewModel {
 
     var onSignInSuccess: ((User) -> Void)?
     var onSignInFailure: ((String) -> Void)?
     var onCreateAccountRequested: (() -> Void)?
+    private var userID: String? {
+        return UserDefaults.standard.string(forKey: "userUID")
+    }
+    private let db = Firestore.firestore()
+    private var cartItems: [Product: Int] = [:]
+
+
     
     var onSignInRequested: (() -> Void)?
     
@@ -49,6 +57,52 @@ class AuthorizationViewModel {
             }
         }
     }
+    
+    func listenToCartChanges() {
+        guard let userID = userID else { return }
+
+        db.collection("users")
+            .document(userID)
+            .collection("cart")
+            .addSnapshotListener { [weak self] snapshot, error in
+                guard let self = self else { return }
+                if let error = error {
+                    print("Error listening for cart changes: \(error.localizedDescription)")
+                    return
+                }
+                guard let documents = snapshot?.documents else { return }
+
+                self.cartItems.removeAll()
+                for document in documents {
+                    let data = document.data()
+                    if let name = data["name"] as? String,
+                       let price = data["price"] as? String,
+                       let quantity = data["quantity"] as? Int {
+
+                        let unit = data["unit"] as? String ?? ""
+                        let featuresGeo = data["featuresGeo"] as? String ?? ""
+                        let category = data["category"] as? String ?? "Unknown"
+                        let link = data["link"] as? String
+                        let supplier = data["supplier"] as? String ?? "Unknown"
+
+                        let product = Product(
+                            name: name,
+                            price: price,
+                            codeID: document.documentID,
+                            unit: unit,
+                            featuresGeo: featuresGeo,
+                            category: category,
+                            link: link,
+                            imageURL: nil,
+                            supplier: supplier
+                        )
+                        self.cartItems[product] = quantity
+                    }
+                }
+            }
+    }
+
+
 
 
     func createAccount(email: String, password: String) {
