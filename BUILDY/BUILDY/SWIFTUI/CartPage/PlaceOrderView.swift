6f3331a93685e55acee0c2c316fb8b0dcde744cb
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-01-21 21:05:28 +0400

    add Place Order Page View

---
+++ BUILDY/BUILDY/SWIFTUI/CartPage/PlaceOrderView.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  BUILDY
+//
+//  Created by Nino Kurshavishvili on 21.01.25.
+//
+
---
+++ BUILDY/BUILDY/SWIFTUI/ProfilePage/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  BUILDY
+//
+//  Created by Nino Kurshavishvili on 21.01.25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
